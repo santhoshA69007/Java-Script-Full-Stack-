1c8c2addcf4eb970c113bf14d5e6435864511330
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-01-20 17:31:02 +0530

    created

---
+++ Java_Script program and projects/jquery/testing the jquery/index.js
@@ -0,0 +1,6 @@
+$("h1").addClass("big-title margin-50");
+$("h1").html("<i>BBYE!</i>");
+$("a").attr("href","https://www.google.com")
+$("button").click(function(){
+  $("h1").css("color","purple");
+});
