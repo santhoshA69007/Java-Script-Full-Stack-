17105c19e22f14cc6af92adf70d88fcec2c54305
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-01-28 16:17:42 +0530

    created

--- Java_Script_program_and_projects/nodejs/my_Express_Server/server.js
+++ Java_Script_program_and_projects/nodejs/my_Express_Server/server.js
@@ -1,2 +1,12 @@
 const express= require("express");
 const app= express();
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+app.get("/about", (req, res) => {
+    res.send("About Page hi iam santhosh with big thing");
+});
+app.get("/abob", (req, res) => {
+    res.send("About Page hi iam santhosh with big bib");
+});
+app.listen(3000, () => console.log("Server is running on this px"));
