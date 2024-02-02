2e6300082c861a3fdd38116ca2ec469057c81d9c
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-02-02 15:15:32 +0530

    created

--- Java_Script_program_and_projects/nodejs/API/mailClimp/app.js
+++ Java_Script_program_and_projects/nodejs/API/mailClimp/app.js
@@ -1,4 +1,17 @@
+//jshint esversion:6
 const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();
 app.use(bodyParser.urlencoded({urlencoded: true}));
+app.use(express.json());
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+})
+app.post("/signup",function(req,res){
+    var email= req.body.email;
+    var password= req.body.password;
+    console.log(email,password);
+})
+app.listen("3000",function(){
+    console.log("sever is runing on port 3000");
+});
