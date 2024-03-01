bf8a2ca41d68f81b27f96b3bc459622bff883a1e
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-03-01 23:22:02 +0530

    Refactor code and add new functionality

--- mongoDB/FruitsProject/app.js
+++ mongoDB/FruitsProject/app.js
@@ -1,37 +1,72 @@
 
 var mongoose = require('mongoose'); // Connection URI
 
-
 var uri = 'mongodb://localhost:27017/fruitsDB'; // Replace with your MongoDB URI
 // Connect to MongoDB
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}); // Get the default connection
+mongoose.connect(uri); // Get the default connection
 
 //structure of database
 const fruitSchema= new mongoose.Schema({
-  name:String,
+  _id:{
+    type:Number,
+    required:true,
+  },
+  name:{
+    type:String,
+    required:true
+  },
   rating:Number,
   review:String
 });
 
 const Fruit=mongoose.model('Fruit', fruitSchema);
-const fruit=new Fruit({
-name:"apple",
-rating:5,
-review:"apple a day keeps the doctor away apple is the biggest enemy in the world for them"
-});
-// fruit.save();
+
 const personSchema=new mongoose.Schema({
+  _id:{
+    type:Number,
+    required:true,
+  },
   name:String,
-  age:Number
+  age:Number,
+  fav_fruit:fruitSchema,
 });
 
 const person=new mongoose.model("Person",personSchema);
 const per_doc=new person({
+  _id:1,
   name:"john",
   age:30
 });
-per_doc.save();
+// per_doc.save();
+
+    const pineapple=new Fruit({
+      _id:4,
+      name:"pineapple",
+      rating:4,
+      review:"like its smell but its spikey!!"
+      });
+
+// Fruit.insertMany([pineapple]);
+async function working(){
+  const res= await Fruit.find({rating:{$gt:1}})
+  res.forEach((fruit)=>{
+    console.log(fruit.rating);
+    console.log("inserted successfully!");
+  })
+  // await person.updateOne({name:"john"},{fav_fruit:pineapple});
+ console.log("updated successfully!");
+}
+working();
+async function working(){
+  const res= await person.find()
+  res.forEach((person)=>{
+    console.log(person);
+
+  })
+  // await person.updateOne({name:"john"},{fav_fruit:pineapple});
+ console.log("updated successfully!");
+}
+working();
+
+
--- mongoDB/FruitsProject/dist/app.dev.js
+++ mongoDB/FruitsProject/dist/app.dev.js
@@ -6,31 +6,73 @@ var mongoose = require('mongoose'); // Connection URI
 var uri = 'mongodb://localhost:27017/fruitsDB'; // Replace with your MongoDB URI
 // Connect to MongoDB
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}); // Get the default connection
+mongoose.connect(uri); // Get the default connection
 //structure of database
 
 var fruitSchema = new mongoose.Schema({
-  name: String,
+  _id: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
   rating: Number,
   review: String
 });
 var Fruit = mongoose.model('Fruit', fruitSchema);
-var fruit = new Fruit({
-  name: "apple",
-  rating: 5,
-  review: "apple a day keeps the doctor away apple is the biggest enemy in the world for them"
-}); // fruit.save();
-
 var personSchema = new mongoose.Schema({
+  _id: {
+    type: Number,
+    required: true
+  },
   name: String,
-  age: Number
+  age: Number,
+  fav_fruit: fruitSchema
 });
 var person = new mongoose.model("Person", personSchema);
 var per_doc = new person({
+  _id: 1,
   name: "john",
   age: 30
-});
-per_doc.save();
+}); // per_doc.save();
+
+var pineapple = new Fruit({
+  _id: 4,
+  name: "pineapple",
+  rating: 4,
+  review: "like its smell but its spikey!!"
+}); // Fruit.insertMany([pineapple]);
+
+function working() {
+  var res;
+  return regeneratorRuntime.async(function working$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(Fruit.find({
+            rating: {
+              $gt: 1
+            }
+          }));
+
+        case 2:
+          res = _context.sent;
+          res.forEach(function (fruit) {
+            console.log(fruit.rating);
+            console.log("inserted successfully!");
+          }); // await person.updateOne({name:"john"},{fav_fruit:pineapple});
+
+          console.log("updated successfully!");
+
+        case 5:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+}
+
+working();
